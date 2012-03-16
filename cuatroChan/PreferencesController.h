0f0b733d7aefbf62ad2ff4684d42531f6026c3bb
Dev Omoikane <issurfer@gmail.com>
2012-03-15 17:13:10 -0700

    * [ALL] First Working version

---
+++ cuatroChan/PreferencesController.h
@@ -0,0 +1,13 @@
+//
+//  PreferencesController.h
+//  cuatroChan
+//
+//  Created by Israel Cabrera on 3/15/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface PreferencesController : NSWindowController
+
+@end
---
+++ cuatroChan/PreferencesController.m
@@ -0,0 +1,30 @@
+//
+//  PreferencesController.m
+//  cuatroChan
+//
+//  Created by Israel Cabrera on 3/15/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "PreferencesController.h"
+
+@implementation PreferencesController
+
+- (id)initWithWindow:(NSWindow *)window
+{
+    self = [super initWithWindow:window];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+- (void)windowDidLoad
+{
+    [super windowDidLoad];
+    
+    // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+}
+
+@end
